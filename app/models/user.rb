25a183d5fec489150017db6220052f032f8d6e95
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-04-29 23:41:55 +0900

    インクリメンタルサーチの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   has_many :massages
 
   validates :name, presence: true, uniqueness: true
+  def self.search(input, id)
+    return nil if input == ""
+    User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+  end
 end
